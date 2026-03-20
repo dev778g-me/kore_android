defb6eef79dbbad5872eb78fe1fb12e77cfa0a44
dev778g-me <sanji778g@gmail.com>
2026-03-20 15:53:01 +0530

    ADD- added circular progress bar

--- korelibrary/src/main/java/com/dev/korelibrary/src/Components/Progress/ProgressBars.kt
+++ korelibrary/src/main/java/com/dev/korelibrary/src/Components/Progress/ProgressBars.kt
@@ -4,6 +4,8 @@ import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.getValue
@@ -11,16 +13,20 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.dev.korelibrary.src.Components.Themes.KoreTheme
+import com.dev.korelibrary.src.Components.Themes.Ripple.Logger
 
 @Composable
 fun LinearProgressBar(
+    progress : Float,
     modifier: Modifier = Modifier,
     thickness : Dp = ProgressDefaults.defaultProgressBarThickness,
+    cap : StrokeCap = ProgressDefaults.defaultProgressBarCap,
     colors: BarProgressColors = ProgressDefaults.barProgressColors(),
-    progress : Float,
 ) {
 
 
@@ -42,9 +48,12 @@ fun LinearProgressBar(
             .height(thickness)
     ) {
         val yCenter = size.height/2
-        val startX = thickness.toPx() / 2
-        val endX = size.width - (thickness.toPx() / 2)
-        val drawableWidth = endX - startX
+        val strokeWidthPx = thickness.toPx()
+
+        val inset = if (cap == StrokeCap.Butt) 0f else strokeWidthPx /2
+
+        val endX = size.width - inset
+        val drawableWidth = endX - inset
 
 
         // track of the barrrrrrrrr
@@ -53,10 +62,10 @@ fun LinearProgressBar(
             cap = StrokeCap.Round,
             color = trackColor,
             strokeWidth = thickness.toPx(),
-            start = Offset(x = 0f, y =  yCenter),
-            end = Offset(x = drawableWidth, y = yCenter)
+            start = Offset(x = inset, y =  yCenter),
+            end = Offset(x = endX, y = yCenter)
         )
-        val progressWidth = drawableWidth * resolvedProgress
+        val progressWidth =( drawableWidth * resolvedProgress) + inset
 
         if (resolvedProgress >0){
 
@@ -64,7 +73,7 @@ fun LinearProgressBar(
                 cap = StrokeCap.Round,
                 color = progressColor,
                 strokeWidth = thickness.toPx(),
-                start = Offset(x = 0f, y =  yCenter),
+                start = Offset(x = inset, y =  yCenter),
                 end = Offset(x = progressWidth, y = yCenter)
             )
         }
@@ -78,6 +87,53 @@ fun LinearProgressBar(
 
 
 }
+
+
+
+
+
+
+@Composable
+fun CircularProgressBar(
+    progress: Float,
+    modifier: Modifier = Modifier,
+    thickness: Dp = ProgressDefaults.defaultCircularBarThickness,
+    size : Dp = ProgressDefaults.defaultCircularProgressBarSize,
+    cap: StrokeCap = ProgressDefaults.defaultProgressBarCap,
+    colors: BarProgressColors = ProgressDefaults.circularProgressColors()
+){
+
+
+    val coercedProgress = (progress /100f).coerceIn(0f..1f)
+    Logger.showLog("${thickness.value}")
+    Canvas(
+        modifier = modifier
+            .size(size)
+            .padding(thickness /2)
+    ) {
+        drawArc(
+            color = colors.trackColor,
+            style = Stroke(width = thickness.value, cap = cap),
+            startAngle =  -90f,
+            sweepAngle =  360f,
+            useCenter = false
+        )
+      drawArc(
+          color = colors.progressColor,
+          style = Stroke(width = thickness.value, cap = cap),
+          startAngle =  -90f,
+          sweepAngle = coercedProgress * 360f,
+          useCenter = false
+      )
+    }
+
+
+}
+
+
+
+
+
 
 
 
@@ -106,6 +162,12 @@ private fun BarProgressColors.progressColor(enabled: Boolean) : Color{
 
 object ProgressDefaults{
 
+
+    val defaultCircularBarThickness = 12.dp
+
+    val defaultCircularProgressBarSize = 48.dp
+
+    val defaultProgressBarCap : StrokeCap = StrokeCap.Round
     val defaultProgressBarThickness = 4.dp
 
    @Composable
@@ -120,5 +182,20 @@ object ProgressDefaults{
         disabledTrackColor = disabledTrackColor,
         disabledProgressColor = disabledProgressColor
     )
+
+    @Composable
+    fun circularProgressColors(
+        trackColor: Color = KoreTheme.colorScheme.backGroundVariant,
+        progressColor: Color = KoreTheme.colorScheme.primary,
+        disabledTrackColor: Color = KoreTheme.colorScheme.disabled,
+        disabledProgressColor: Color = KoreTheme.colorScheme.onDisabled,
+    ) = BarProgressColors(
+        trackColor = trackColor,
+        progressColor = progressColor,
+        disabledTrackColor = disabledTrackColor,
+        disabledProgressColor = disabledProgressColor
+    )
+
+
 
 }
