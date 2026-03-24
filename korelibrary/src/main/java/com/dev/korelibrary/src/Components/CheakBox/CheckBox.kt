5cb2626d05feda097808ac7e3ac8530e8633d5ee
dev778g-me <sanji778g@gmail.com>
2026-03-25 03:06:37 +0530

    REFACTOR - refactor checkbox

--- korelibrary/src/main/java/com/dev/korelibrary/src/Components/CheakBox/CheckBox.kt
+++ korelibrary/src/main/java/com/dev/korelibrary/src/Components/CheakBox/CheckBox.kt
@@ -68,6 +68,7 @@ fun CheckBox(
 
 
     val toggleModifier = Modifier.toggleable(
+        enabled = enabled,
         value = checked,
         onValueChange = onCheckChange,
         role = Role.Checkbox,
